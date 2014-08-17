f75511d11825888e7177194639a578b64c6b4365
rodionovd <i.am.rodionovd@gmail.com>
2014-08-17 17:43:17 +0700

    Push a framework boilerplate

---
+++ HSCore/HSCore.h
@@ -0,0 +1,19 @@
+//
+//  HSCore.h
+//  HSCore
+//
+//  Created by Dmitry Rodionov on 8/17/14.
+//  Copyright (c) 2014 rodionovd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HSCore.
+FOUNDATION_EXPORT double HSCoreVersionNumber;
+
+//! Project version string for HSCore.
+FOUNDATION_EXPORT const unsigned char HSCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSCore/PublicHeader.h>
+
+
